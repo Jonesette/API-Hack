76fa2321d997ef15bacc7acbc55ad3a0f340254c
Jonesette <sjones@jjaviation.com>
2014-06-28 23:30:56 -0600

    Beginnings of API Query
    API functioning but results not parsing correctly.

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-$(document).ready( function() {
-
-	var selectedGame = function() {
-		$("input:radio[name='gameList']:checked").val();
-	}
-
-	
-
+$(document).ready(function() {
+	getTeams();
 });
+
+var getTeams = function() {
+	$("#teamSelection").html("Loading...");
+	$.ajax({
+		url: "http://api.espn.com/v1/sports/baseball/mlb/teams?apikey=6xczn8m4pu3se2vj62r9jace",	
+		dataType: "jsonp",
+		type: 'GET',
+		complete: function() {
+			$("#teamSelection").html("Select a Team...");
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log(jqXHR);
+			console.log(textStatus);
+			console.log(errorThrown);
+		},
+		success: function(result) {
+			$.each(result.sports.leagues.teams, function(i,item) {
+				$("#teams").append("<option class=\"team\">"+item.name+"</option>");
+			});
+		}
+	});
+};	
+
+
+
+
+
+
+
